11e8d9e9628fab9cd2b55ee58aa530f00e2c2494
Paulson <paulsonmsc@gmail.com>
2024-09-16 15:17:58 +0530

    Sign-up page creation

--- sample/src/App.jsx
+++ sample/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+function App(){
   return (
-    <>
-      
+      <>
+        <div className='h-screen w-screen flex justify-center items-center bg-gradient-to-r from-black to-blue-500'>
+          <div className='h-3/5 w-1/3 bg-black flex flex-col justify-center items-center rounded-lg'>
+          <h1 className='text-white font-bold text-3xl'>Sign-Up</h1>
+          <form className='w-[80%] h-[80%] flex flex-col justify-center items-center gap-5'>
+            <input type='text' name='' id='' placeholder='Name' className='p-3 outline-none active:outline-none focus:outline-blue-500 bg-gray-600 text-white rounded-md w-full'></input>
+            <input type='text' name='' id='' placeholder='E-mail' className='p-3 outline-none active:outline-none focus:outline-blue-500 bg-gray-600 text-white rounded-md w-full'></input>
+            <input type='tel' name='' id='' placeholder='Phone' className='p-3 outline-none active:outline-none focus:outline-blue-500 bg-gray-600 text-white rounded-md w-full'></input>
+            <input type='password' name='' id='' placeholder='Password' className='p-3 outline-none active:outline-none focus:outline-blue-500 bg-gray-600 text-white rounded-md w-full'></input>
+            <br></br>
+            <button type='submit' className='w-[30%] bg-blue-900 text-white p-3 rounded-md'>Submit</button>
+          </form>
+          </div>
+        </div>
     </>
   )
 }
