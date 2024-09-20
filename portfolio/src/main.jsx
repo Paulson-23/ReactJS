73c61c5c828efcd97697068078ff0ad07a73a95b
Paulson <paulsonmsc@gmail.com>
2024-09-20 10:45:06 +0530

    Added technologies known in Home.jsx

--- portfolio/src/main.jsx
+++ portfolio/src/main.jsx
@@ -2,12 +2,9 @@ import App from './App'
 import './assets/css/style.css'
 import { createRoot } from 'react-dom/client'
 import {BrowserRouter} from 'react-router-dom'
-import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')).render(
-    // <StrictMode>
     <BrowserRouter>
         <App />
     </BrowserRouter>
-    // </StrictMode>
 ) 
--- portfolio/src/pages/Home.jsx
+++ portfolio/src/pages/Home.jsx
@@ -1,11 +1,10 @@
-import Navbar from "../components/Navbar"
 import Profile from "../assets/img/passport-size-photo.jpg"
 const Home = () =>{
     return(
       <div className=" h-screen flex flex-col justify-between items-center ">
         {/* <Navbar></Navbar> */}
         <div className="w-full h-full flex flex-row justify-around items-center ">
-          <div className="flex flex-col justify-center text-white gap-10">
+          <div className="flex flex-col text-white gap-10">
             <div className="text-5xl">
               Paulson M S
             </div>
@@ -14,6 +13,15 @@ const Home = () =>{
               HTML and CSS languages.Experienced in designing and implementing scalable solutions for projects. <br></br>
               Strong problem-solving abilities coupled with effective communication skills. <br></br>
               Committed to continuous learning and staying updated with emerging technologies.
+            </div>
+            <div className="h-20 items-center">
+            <div className="flex justify-around">
+              <button className="flex"><i className="fa-brands fa-java text-5xl hover:text-7xl transition-all"></i></button>
+              <button className="flex"><i className="fa-brands fa-react text-5xl hover:text-7xl transition-all"></i></button>
+              <button className="flex"><i className="fa-brands fa-html5 text-5xl hover:text-7xl transition-all"></i></button>
+              <button className="flex"><i className="fa-brands fa-css3-alt text-5xl hover:text-7xl transition-all"></i></button>
+              <button className="flex"><i className="fa-solid fa-database text-5xl hover:text-7xl transition-all"></i></button>
+            </div>
             </div>
           </div>
           <div className="h-[40vh] w-[20vw] flex flex-col justify-center">
