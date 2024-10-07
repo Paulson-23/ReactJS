e02584b0bcc35a5f6aa0352a93bdb03b7171fda2
Paulson <paulsonmsc@gmail.com>
2024-10-07 19:39:55 +0530

    created more routesin backend

--- portfolio/backend/routes/projects.js
+++ portfolio/backend/routes/projects.js
@@ -23,4 +23,31 @@ router.route('/add').post((req,res) => {
 });
 
 
+router.route('/:id').delete((req,res) => {
+    Projects.findByIdAndDelete(req.params.id)
+    .then(projects => res.json('Project deleted!'))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').get((req,res) => {
+    Projects.findById(req.params.id)
+    .then(projects => res.json(projects))
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
+router.route('/:id').post((req,res) => {
+    Projects.findById(req.params.id)
+    .then(projects => {
+        projects.Title = req.body.Title;
+        projects.Desc = req.body.Desc;
+        projects.Cover = req.body.Cover;
+        projects.Link = req.body.Link;
+
+        projects.save()
+            .then(() => res.json("Project updated!"))
+            .catch(err => res.status(400).json('Error: ' + err));
+    })
+    .catch(err => res.status(400).json('Error: ' + err));
+});
+
 module.exports = router;
